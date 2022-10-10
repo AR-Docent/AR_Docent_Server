8e939f352a73a67d19a745e087b2ec3f93fdc99d
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-10-11 01:33:43 +0900

    start asp.net 3.1 web application server

---
+++ AR_Docent.website/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AR_Docent.website.Services;
+using AR_Docent.website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AR_Docent.website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public JsonFileProductsService ProductsService;
+        public IEnumerable<Product>Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductsService productsService)
+        {
+            _logger = logger;
+            ProductsService = productsService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductsService.GetProducts();
+        }
+    }
+}
---
+++ AR_Docent.website/Services/JsonFileProductsService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using AR_Docent.website.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AR_Docent.website.Services
+{
+    public class JsonFileProductsService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; private set; }
+
+        public JsonFileProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            IEnumerable<Product> products = GetProducts();
+            Product query = products.First(x => x.ID == productId);
+
+            if (query.Rating == null)
+            {
+                query.Rating = new int[] { rating };
+            }
+            else
+            {
+                List<int> ratings = query.Rating.ToList();
+                ratings.Add(rating);
+
+                query.Rating = ratings.ToArray<int>();
+            }
+
+            using (FileStream outputStream = File.Open(JsonFileName, FileMode.Truncate))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products);
+            }
+        }
+    }
+}
