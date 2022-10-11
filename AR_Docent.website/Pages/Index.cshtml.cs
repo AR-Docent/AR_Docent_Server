0375abc189323041dc0259ad4f5cb3fbf11464f1
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-10-12 00:51:35 +0900

    예제 완료

--- AR_Docent.website/Pages/Index.cshtml.cs
+++ AR_Docent.website/Pages/Index.cshtml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AR_Docent.website.Services;
 using AR_Docent.website.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
