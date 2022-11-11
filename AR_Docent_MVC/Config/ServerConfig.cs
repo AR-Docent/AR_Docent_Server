c579d6f39a5312c1fa8050b559d65cac49b9a927
성현 홍 <hongsam14@gmail.com>
2022-11-11 16:54:49 +0900

    new project is MVC pattern

---
+++ AR_Docent_MVC/Config/ServerConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Common;
+
+namespace AR_Docent_MVC.Config
+{
+    public class ServerConfig
+    {
+        //azure keyvault url
+        public static readonly string keyVaultUrl = "https://arstorageaccess.vault.azure.net/";
+        //sql config
+        public static readonly string connectionStringSql = "ConnectionStrings--Sql";
+        //blob storage config
+        public static readonly string connectionStringBlob = "ConnectionString--Blob";
+        //blob storage audio
+        public static readonly string imgContainerName = "arimage";
+        //blob storage audio config
+        public static readonly string connectionStringAudio = "ConnectionString--Speech";
+        public static readonly string audioContainerName = "araudio";
+        public static readonly string region = "koreacentral";
+    }
+}
