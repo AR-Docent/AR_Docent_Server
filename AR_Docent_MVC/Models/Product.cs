8cccc3953a053ef41cfa6cba02a529dfd1e5fa84
성현 홍 <hongsam14@gmail.com>
2023-02-27 02:17:30 +0900

    change unityinfo model, add error detection to create view page

--- AR_Docent_MVC/Models/Product.cs
+++ AR_Docent_MVC/Models/Product.cs
@@ -10,18 +10,34 @@ namespace AR_Docent_MVC.Models
     public class Product
     {
         public int id { get; set; }
+        
+        [Required]
         public string name { get; set; }
+        
+        [Required]
         public string title { get; set; }
+        
+        [Required]
         [NotMapped]
         public IFormFile image { get; set; }
+        
         [NotMapped]
         public AudioDataStream audio { get; set; }
+        
+        [Required]
+        [StringLength(500)]
         public string content { get; set; }
+        
         [DataType(DataType.DateTime)]
         public DateTime created_at { get; set; }
+        
         public string img_name { get; set; }
+        
         public string audio_name { get; set; }
+        
+        [Required]
         public int img_width { get; set; }
+        [Required]
         public int img_height { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
